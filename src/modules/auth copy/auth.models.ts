ff91b754bc8fee4e5cf52b1c5be9d4a96c87a4d1
slava <vyacheslavkolodrubskiy@gmail.com>
2023-03-24 09:34:50 +0600

    chore: update

--- src/modules/auth copy/auth.models.ts
+++
@@ -1,6 +0,0 @@
-export interface Form {
-  phone: string
-  password: string
-}
-
-export interface RequestForm extends Pick<Form, 'phone'> {}
---
+++ src/modules/profile/stores/profile.store.ts
@@ -0,0 +1,9 @@
+import { defineStore } from 'pinia'
+
+interface ProfileState {}
+
+export const useProfileStore = defineStore('profile', {
+  state: (): ProfileState => ({}),
+  getters: {},
+  actions: {},
+})
